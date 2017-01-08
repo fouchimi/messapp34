4d5991a9fec983261e301b228f116455065eaee8
Ousmane <ousmanefouchimi1990@gmail.com>
2017-01-08 02:04:10 -0700

    Change SERVER URL

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'LHkNxyEd8sTQmUtNxyEdsBJaUv',
   masterKey: process.env.MASTER_KEY || 'kBxQmIsA43P9Oo73Sa1n9', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://messapp34.herokuapp.com/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://messapp34.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
