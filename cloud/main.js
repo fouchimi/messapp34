159163430d5c2b080e8d33f779ac7d2454179096
Ousmane <ousmanefouchimi1990@gmail.com>
2017-01-16 22:14:16 -0700

    Adding cloud code for notification handling

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,33 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+// Android push test
+Parse.Cloud.define('chatChannel', function(request, response) {
+
+  // request has 2 parameters: params passed by the client and the authorized user
+  var params = request.params;
+  var user = request.user;
+  
+  var sender = params.sender;
+  var receiver = params.receiver;
+  var message = params.message;
+
+  var payload = {"sender" : sender, "receiver" : receiver, "message" : message};
+
+  // use to custom tweak whatever payload you wish to send
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo("deviceType", "android");
+
+
+  // Note that useMasterKey is necessary for Push notifications to succeed.
+
+  Parse.Push.send({
+  where: pushQuery,      // for sending to a specific channel
+  data: payload,
+  }, { success: function() {
+     console.log("#### PUSH OK");
+  }, error: function(error) {
+     console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
 });
+
