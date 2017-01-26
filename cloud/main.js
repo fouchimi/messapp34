76d70952f04ae6034e7e7ea36abceca67609d9a9
Ousmane <ousmanefouchimi1990@gmail.com>
2017-01-25 20:10:51 -0700

    Modify cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,14 +1,14 @@
 
 // Android push test
-Parse.Cloud.define('sendPushToUser', function(request, response) {
-  var sender = request.params.sender;
+Parse.Cloud.define('Chats', function(request, response) {
+  var sender = request.params.title;
   var receiver = request.params.receiver;
-  var message = request.params.message;
+  var message = request.params.alert;
 
   // use to custom tweak whatever payload you wish to send
   var pushQuery = new Parse.Query(Parse.Installation);
   pushQuery.equalTo("deviceType", "android");
-  pushQuery.equalTo("user", receiver);
+  //pushQuery.equalTo("user", receiver);
 
   var payload = {"title" : sender, 
                  "alert": message };
