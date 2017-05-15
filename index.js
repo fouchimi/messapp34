fc749adaba0266f28143285139722224720e3089
Fouchimi <ousmanefouchimi@gmail.com>
2017-05-15 10:56:04 -0600

    Change heroku databaseUri in index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if (process.env.GCM_SENDER_ID && process.env.GCM_API_KEY) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://heroku_631dvgmh:hjcpehpi69k4d2ld4mjf024q85@ds157288.mlab.com:57288/heroku_631dvgmh',
+  databaseURI: databaseUri || 'mongodb://LeBoss:Heroku984178@ds157288.mlab.com:57288/heroku_631dvgmh',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'LHkNxyEd8sTQmUtNxyEdsBJaUv',
   masterKey: process.env.MASTER_KEY || 'kBxQmIsA43P9Oo73Sa1n9', //Add your master key here. Keep it secret!
